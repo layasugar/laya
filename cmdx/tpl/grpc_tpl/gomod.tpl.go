604e9b0f100c0c3fb725c3e3a7d337036c26927d
laya <laya.sugar@gmail.com>
2022-02-12 14:15:19 +0800

    fixfix

--- cmdx/tpl/grpc_tpl/gomod.tpl.go
+++ cmdx/tpl/grpc_tpl/gomod.tpl.go
@@ -4,11 +4,13 @@ const GoModTpl = `module {{.goModName}}
 
 go 1.17
 
-require github.com/urfave/cli/v2 v2.3.0
-
 require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
-	github.com/russross/blackfriday/v2 v2.0.1 // indirect
-	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/go-redis/redis/v8 v8.11.4
+	github.com/layasugar/laya v0.0.1
+	github.com/opentracing/opentracing-go v1.2.0
+	github.com/uber/jaeger-client-go v2.30.0+incompatible
+	google.golang.org/grpc v1.44.0
+	google.golang.org/protobuf v1.27.1
+	gorm.io/gorm v1.22.5
 )
 `
